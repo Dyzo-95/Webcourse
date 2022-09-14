2b4dfa0e9408f2039a342f2b08db1c573dbf6e95
Dyzo-95 <59065460+Dyzo-95@users.noreply.github.com>
2022-09-14 02:34:54 +0200

    Create qsn.php

---
+++ qsn.php
@@ -0,0 +1,38 @@
+<?php  $title = 'Webcourse'; 
+        require ('app/header.php');
+?>
+<html>
+    <div class="Qui">
+        <h1 class="slogan"> Ensemble, gardons la forme </h1>
+
+        <body class="bd-qsn">
+            <div class="hist">
+
+                <h2 class="Histoire"> Notre Histoire </h2>
+
+                <div class="htext">
+                    <p class="histext"> Association de la loi 1901 créée en 1960, par deux frères passionnés de courses, ….  Et c’est là que tout a commencé ….. Enchaînement d’actions 
+                        Aujourd’hui, notre association compte plus d’un million de membres, dont dix mille  professionnels répartis dans diverses catégories comme la courses à pied, le vélo, la natation,  et parmi lesquels nous retrouvons des champions tels que …… 
+                    </p>
+                </div>
+                <div class="jac">
+                    <img src="assets/img/Jacule.jpeg" class="Jacule">
+                    <ul class="info">
+                        <li> 91, Chemin Challet 75001 Paris </li>
+                        <li> 01.09.47.43.02, webcourse@gmail.com </li>
+                    </ul>
+                </div>
+            </div>
+        </body>
+    
+        <footer class="footqsn">
+
+            <a href="app/Mention2.pdf" class="Mention"> Mentions Légales </a>
+
+            <a href="app/Politique de confidentialité.pdf" class="Politique"> Politique de confidentialité </a> <a href="app/Charte sur les cookies.pdf" class="Charte"> Charte sur les cookies </a>
+
+            <a href="app/autorisation-photo.pdf" class="Autorisation"> Droit à l'image </a>
+        </footer>
+    </div>   
+
+</html>
