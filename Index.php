8f2b9c7663353a46f409b9ae9dc0422397db5f09
Dyzo-95 <59065460+Dyzo-95@users.noreply.github.com>
2022-09-14 02:33:22 +0200

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,24 @@
+<?php  $title = 'Webcourse'; 
+        require ('app/header.php');
+?>
+<html>
+	
+	<h1 class="bvn">Le site des courses pour amateurs et professionnels</h1>
+
+
+	<img src="assets/img/course.png" class="Course" alt="Course">
+		<body class="bg-green">
+	
+			<p class="indextext">
+			Ce site est dédié aux coureurs amateurs et professionnels cherchant à participer à différentes courses au niveau national.<br/>
+            Pour vous aidez, un calendrier évènementiel présentant les courses par régions, dates et lieux à votre disposition.<br/>
+            Des informations par club vous sont présentés (nombre d'adhérants, classement).<br/>
+            <br/>Dans votre espace personnel, vous trouverez le bulletin d'inscription pour la participation à un évènement, vos statistiques de courses, vos classements.
+                        
+                
+			</p>
+			<footer class="footindex">
+			Pour nous contacter, vous pouvez utiliser l'onglet contact ou bien nous joindre par email à evenementiel_sio@sitewebdescourses.fr
+			</footer>
+		</body>
+</html>
