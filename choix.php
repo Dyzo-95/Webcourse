3d54e1a3fb1ab97817c7347173b18d4e6c51f4fb
Dyzo-95 <59065460+Dyzo-95@users.noreply.github.com>
2022-09-14 02:48:00 +0200

    Create choix.php

---
+++ choix.php
@@ -0,0 +1,16 @@
+<?php  $title = 'Choix'; 
+        require ('app/header.php');
+        require ('app/function.php');
+        require ('app/connect.php');
+?>
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+        <h1> Est-ce la première fois que vous vous inscrivez à un évènement? </h1>
+
+        <a href="evenement.php" class="OUI"> OUI </a>
+        <a href="epreuve.php" class="NON"> NON </a>
+    </body>
+</html>
