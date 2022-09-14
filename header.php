c41f7d7fc6436ce7fee01fd084891065bf84cf68
Dyzo-95 <59065460+Dyzo-95@users.noreply.github.com>
2022-09-14 03:04:38 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="utf-8"  />
+        <title><?=$title?></title>
+        <link rel="stylesheet" href="assets/styles/nav.css">
+		<link rel="stylesheet" href="assets/styles/main.css">
+	</head>
+	<body>
+			<?php require('nav.php'); ?>
